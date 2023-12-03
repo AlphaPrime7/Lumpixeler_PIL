50c6bf9c7982f902822efa89f628ccca9590f5c0
alphaprime7 <awesome.tingwei@outlook.com>
2023-12-03 05:14:41 -0600

    up-the little weekend gig

--- Py/enhsheet.py
+++ Py/enhsheet.py
@@ -1,4 +1,4 @@
-'''The `contact_sheet` function creates a contact sheet which is a PIL.Image.Image object with multiple images in sequence.
+'''The `enhsheet` function creates a contact sheet which is a PIL.Image.Image object with multiple images that have varying parameters.
    
    Parameters
     ----------
@@ -6,24 +6,28 @@
     The 'ipath' parameter is the image path (URL or local path) pointing directly to the image.
     numimages: numeric
     The 'numimages' parameter refers to the number of images the user wants on the contact sheet.
+    enh_scale: numeric
+    The 'enh_scale' parameter determines image brightness. A lower value leads to peak brightness.
     sheet_mode: str
     This refers to the form of the contact sheet. In 'long' mode means height > width; 'wide' mode means width > height; 'balanced' means width = height.
     ncols: numeric
     The number of columns used when the sheet_mode is balanced.
+    enh_type: str
+    A string for the enhancement feature. Has 4 features.
     
     Returns
     -------
     PIL.Image.Image
-        The function `contact_sheet` returns a PIL.Image.Image object.
-        Chande Momentum Oscillator (CMO) values added.
+        The function `enhsheet` returns a PIL.Image.Image object.
         
     Notes
     -----
-    An adopted function from Christopher Brooks from the University of Michigan.
+    A function created with code adaptations from Christopher Brooks from the University of Michigan.
     Part of the UMich Python specialization.
     
     References:
     1. https://www.coursera.org/learn/python-project/lecture/gDshw/additional-pil-functions
+    
     Examples
     --------
     ```{python}
@@ -31,11 +35,12 @@
     masseff = os.getenv('mass_effect')
     
     # Example - Get a contact sheet
-      contact_sheet(masseff,numimages = 10, sheet_mode = 'long', ncols = 2)
+      enhsheet(masseff,numimages = 10, enh_scale = 5, sheet_mode = 'long', ncols = 2, enh_type= None)
+      enhsheet(ipath = masseff,numimages = 16, enh_scale = 5, sheet_mode = 'balanced', ncols=3, enh_type= 'contrast')
    ```
 '''
 
-def contact_sheet(ipath, numimages, sheet_mode, ncols=None):
+def enhsheet(ipath, numimages, enh_scale, sheet_mode, ncols=None, enh_type= None):
   from PIL import ImageEnhance
   from PIL import Image
   import math
@@ -47,19 +52,30 @@ def contact_sheet(ipath, numimages, sheet_mode, ncols=None):
   imw = img.width
   imh = img.height
   
-  #PIL.ImageEnhance.Brightness object
   enhancer = ImageEnhance.Brightness(img)
+
+  #PIL.ImageEnhance.Brightness object
+  if enh_type == 'brightness' or type is None:
+    enhancer = ImageEnhance.Brightness(img)
+  elif enh_type == 'contrast':
+    enhancer = ImageEnhance.Contrast(img)
+  elif enh_type == 'color':
+    enhancer = ImageEnhance.Color(img)
+  elif enh_type == 'sharpness':
+    enhancer = ImageEnhance.Sharpness(img)
   
   #get the template image
   images = []
   for i in range(0, numimages):
-    images.append(enhancer.enhance(i/numimages))
+    images.append(enhancer.enhance(i/enh_scale))
   template_image = images[0]
+
+  images = images[1:]
   
   if sheet_mode == 'long':
     contact_sheet = Image.new(template_image.mode, (template_image.width, numimages*template_image.height) )
     curr_y = 0
-    for image in images[1:]:
+    for image in images:
       contact_sheet.paste(image, (0, curr_y) ) #xy
       curr_y = curr_y + imh
     contact_sheet = contact_sheet.resize( (int(imh/3),int(imw)) )
@@ -68,7 +84,7 @@ def contact_sheet(ipath, numimages, sheet_mode, ncols=None):
   elif sheet_mode == 'wide':
     contact_sheet = Image.new(template_image.mode, (numimages*template_image.width, template_image.height) )
     curr_x = 0
-    for image in images[1:]:
+    for image in images:
       contact_sheet.paste(image, (curr_x, 0) )
       curr_x = curr_x + imw
     contact_sheet = contact_sheet.resize(( int(imh),int(imw/3) ))
@@ -81,7 +97,7 @@ def contact_sheet(ipath, numimages, sheet_mode, ncols=None):
     contact_sheet = Image.new(template_image.mode, (ncols*template_image.width, nrows*template_image.height) )
     curr_x = 0
     curr_y = 0
-    for image in images[1:]:
+    for image in images:
       contact_sheet.paste(image, (curr_x, curr_y) )
       if curr_x + template_image.width == contact_sheet.width:
         curr_x = 0
@@ -94,5 +110,5 @@ def contact_sheet(ipath, numimages, sheet_mode, ncols=None):
     
 masseff = os.getenv('mass_effect')
 
-cs = contact_sheet(ipath = masseff,numimages = 10, sheet_mode = 'balanced', ncols=3)
+enhsheet(ipath = masseff,numimages = 16, enh_scale = 5, sheet_mode = 'balanced', ncols=3, enh_type= None)
 
---
+++ Py/lumpixeler.py
@@ -0,0 +1,286 @@
+'''The `pixelmod` function creates a PIL.Image.Image object with modified pixels.
+   
+   Parameters
+    ----------
+    img : str
+    A string representing an image.
+    
+    channel: numeric
+    The channel number. The class within the function has the definitions.
+    
+    lum: numeric
+    The intensity value used in pixel manipulation.
+    
+    image_mode: str
+    A string 'RGB' or 'RGBA' used to determine the use of the alpha channel in 
+    pixel manipulation. This parameter is also part of the 'lumpixeler' function.
+    
+    Returns
+    -------
+    PIL.Image.Image
+        The function `pixelmod` returns a PIL.Image object with modified pixels.
+        
+    Notes
+    -----
+    The best part of learning how to manipulate pixels at a higher-level meaning withoug seeing 
+    actual coordinates using numpy.
+    
+    References:
+    1. https://www.coursera.org/learn/python-project/lecture/gDshw/additional-pil-functions
+    Examples
+    --------
+    ```{python}
+    
+    # Example - Pixel extraction and manipulation.
+      pixelmod(img, channel=1, lum=0.5, image_mode='RGBA')
+'''
+
+def pixelmod(img, channel, lum, image_mode = None):
+  import PIL
+  
+  from PIL import Image
+  from PIL import ImageDraw
+  from enum import Enum
+  
+  class ChannelCode(Enum):
+    red = 0
+    green = 1
+    blue = 2
+    alpha = 3
+    
+  width, height = img.size
+    
+  blank_image = Image.new(img.mode, (width, height))
+  
+  for row in range(height):
+    for col in range(width):
+      
+      p = img.getpixel( (col, row) )
+      
+      if channel == ChannelCode.red.value:
+        if image_mode == 'RGBA':
+          blank_image.putpixel( (col, row), ( int(p[0]*lum), p[1], p[2], int(p[3]*lum)  ))
+        else:
+          blank_image.putpixel( (col, row), ( int(p[0]*lum), p[1], p[2] ))
+        
+      elif channel == ChannelCode.green.value:
+        if image_mode == 'RGBA':
+          blank_image.putpixel( (col, row), (p[0], int(p[1]*lum), p[2], int(p[3]*lum) ))
+        else:
+          blank_image.putpixel( (col, row), (p[0], int(p[1]*lum), p[2] ))
+          
+      elif channel == ChannelCode.blue.value:
+        if image_mode == 'RGBA':
+          blank_image.putpixel( (col, row), (p[0], p[1], int(p[2]*lum), int(p[3]*lum) ))
+        else:
+          blank_image.putpixel( (col, row), (p[0], p[1], int(p[2]*lum) ))
+        
+  return(blank_image)
+
+'''The `rendertext` renders text to a PIL object..
+   
+    Parameters
+    ----------
+    image : str
+    A string for the image of interest.
+    
+    channel: numeric
+    The channel number. The class within the function has the definitions.
+    
+    lum: numeric
+    The intensity value used in pixel manipulation.
+    
+    font_num: numeric
+    A number representing the font family. Also used in the parent function. 
+    A limited number of choices are given.
+    
+    Returns
+    -------
+    PIL.Image.Image
+        The function `rendertext` returns a PIL object with rendered text.
+        
+    Notes
+    -----
+    A cool function to help render text to images for labeling purposes.  Think of this 
+    as a way of adding labels to images just as is done for a data frame.
+    
+    References:
+    1. https://www.coursera.org/learn/python-project/lecture/gDshw/additional-pil-functions
+    Examples
+    --------
+    ```{python}
+    
+    # Example - Get a contact sheet
+      rendertext(image, channel=1, lum=0.6, font_num=2)
+'''
+
+def rendertext(image, channel, lum, font_num):
+  
+  import PIL
+  from PIL import Image
+  
+  from PIL import ImageDraw
+  from PIL import ImageFont
+  from urllib.request import urlopen
+  
+  from enum import Enum
+  
+  class FontCode(Enum):
+    fanwood = 1
+    emotional = 2
+    free = 3
+    courier = 4
+    
+  width, height = image.size
+  
+  if font_num == FontCode.fanwood.value:
+    text = "channel {} lum {}".format(channel, lum)
+    font_url = 'https://github.com/AlphaPrime7/pilfonts/blob/main/fanwood/Fanwood-Italic.otf?raw=true'
+    #font = ImageFont.FreeTypeFont(font=r"fanwood\Fanwood-Italic.otf", size=50)
+    font = ImageFont.FreeTypeFont(font=urlopen(font_url), size=100)
+    text_draw_obj = ImageDraw.Draw(image)
+    text_draw_obj.text( ( int(width/2), height ), text, fill='white',font=font, align='left' )  
+  
+  elif font_num == FontCode.emotional.value:
+    text = "channel {} lum {}".format(channel, lum)
+    font_url = 'https://github.com/AlphaPrime7/pilfonts/blob/main/emotional-rescue-font/EmotionalRescue.ttf?raw=true'
+    font = ImageFont.truetype(font=urlopen(font_url), size=100)
+    text_draw_obj = ImageDraw.Draw(image)
+    text_draw_obj.text( ( int(width/2), height ), text, fill='white',font=font, align='left' )  
+  
+  elif font_num == FontCode.free.value:
+    text = "channel {} lum {}".format(channel, lum)
+    font_url = 'https://github.com/AlphaPrime7/pilfonts/blob/main/feelfree-font/FeelfreePersonalUse.ttf?raw=true'
+    font = ImageFont.truetype(font=urlopen(font_url), size=100)
+    text_draw_obj = ImageDraw.Draw(image)
+    text_draw_obj.text( ( int(width/2), height ), text, fill='white',font=font, align='left' )  
+    
+  elif font_num == FontCode.courier.value:
+    text = "channel {} lum {}".format(channel, lum)
+    font_url = 'https://github.com/AlphaPrime7/pilfonts/blob/main/courier-prime-code/courier-prime-code-italic.ttf?raw=true'
+    font = ImageFont.truetype(font=urlopen(font_url), size=100)
+    text_draw_obj = ImageDraw.Draw(image)
+    text_draw_obj.text( ( int(width/2), height ), text, fill='white',font=font, align='left' )  
+
+'''The `lumpixeler` function creates a contact sheet which is a PIL.Image.Image object with images with difference luminescence.
+   
+   Parameters
+    ----------
+    ipath : str
+    The 'ipath' parameter is the image path (URL or local path) pointing directly to the image.
+    
+    lums: tuple
+    The 'lums' parameter is a tuple of intensity or luminescence values.
+    
+    sheet_mode: str
+    This refers to the form of the contact sheet. In 'long' mode means height > width; 
+    'wide' mode means width > height; 'balanced' means width = height.
+    
+    image_mode: str
+    A string either "RGB" or "RGBA". Essentially limiting this function to these image modes.
+    
+    font_num: numeric
+    A parameter that allows the program to choose amongs a selection of fonts to use.
+    
+    ncols: numeric
+    The number of columns used when the sheet_mode is balanced.
+    
+    Returns
+    -------
+    PIL.Image.Image
+        The function `lumpixeler` returns a PIL.Image.Image object.
+        
+    Notes
+    -----
+    A great learning function that finally teaches me pixel manipulation and image data structures.
+    
+    References:
+    1. https://www.coursera.org/learn/python-project/lecture/gDshw/additional-pil-functions
+    Examples
+    --------
+    ```{python}
+
+    masseff = "C://Users//GrandProf//Pictures//mass.effect//me1.png"
+    
+    # Example - Get images with different luminescence
+      lums = [0.1,0.5,0.9]
+      lumpixeler(ipath, lums = lums, image_mode = 'RGB', sheet_mode = 'balanced', font_num=1, ncols = 2)
+'''
+
+def lumpixeler(ipath, lums, sheet_mode, image_mode = None, font_num = None, ncols=None):
+  import PIL
+  from PIL import Image
+  from PIL import ImageDraw
+  import math
+  
+  if image_mode == 'RGB' or image_mode is None:
+    with Image.open(ipath).convert('RGB') as img:
+      img.load()
+  else:
+    with Image.open(ipath) as img:
+      img.load()
+    
+  if image_mode == 'RGB' or image_mode is None:
+    channels = [0,1,2]
+  else:
+    channels = [0,1,2,3]
+    
+  if font_num is None:
+    font_num = 1
+      
+  width, height = img.size
+  
+  image_with_text = Image.new(img.mode, (width, (height+100) ) )
+  image_with_text.paste(img, (0,0))
+  
+  imw, imh = image_with_text.size
+  
+  images = []
+
+  for channel in channels:
+    for lum in lums:
+      images.append(pixelmod(image_with_text, channel, lum, image_mode = image_mode))
+      rendertext(images[-1], channel, lum, font_num=font_num)
+      
+  numimages = len(images)
+  
+  if sheet_mode == 'long':
+    canvas = Image.new(img.mode, (image_with_text.width, numimages*image_with_text.height) )
+    curr_y = 0
+    for image in images:
+      canvas.paste(image, (0, curr_y) ) #xy
+      curr_y = curr_y + imh
+    canvas = canvas.resize( (int(imh/3),int(imw)) )
+    canvas.show()
+    
+  elif sheet_mode == 'wide':
+    canvas = Image.new(img.mode, (numimages*image_with_text.width, image_with_text.height) )
+    curr_x = 0
+    for image in images:
+      canvas.paste(image, (curr_x, 0) )
+      curr_x = curr_x + imw
+    canvas = canvas.resize(( int(imh),int(imw/3) ))
+    canvas.show()
+    
+  elif sheet_mode == 'balanced' and numimages > 2:
+    if ncols is None:
+      ncols = 1
+    nrows = math.ceil(len(images)/ncols)
+    canvas = Image.new(img.mode, (ncols*image_with_text.width, nrows*image_with_text.height) )
+    curr_x = 0
+    curr_y = 0
+    for image in images:
+      canvas.paste(image, (curr_x, curr_y) )
+      if curr_x + image_with_text.width == canvas.width:
+        curr_x = 0
+        curr_y = curr_y + image_with_text.height
+      else:
+        curr_x = curr_x + image_with_text.width
+        
+    canvas = canvas.resize((int(canvas.width/ncols),int(canvas.height/nrows)))
+    canvas.show()
+
+masseff = os.getenv('mass_effect')
+
+lumpixeler(ipath = masseff, lums = (0.1,0.5,0.9), sheet_mode = 'balanced', image_mode = 'RGB', font_num = 1, ncols=1)
+
